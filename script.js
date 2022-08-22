7f2aa238362c62c4e93eb4282c2e0412542e52f0
rizki <rzkalqdr@gmail.com>
2022-08-22 15:20:03 +0700

    adding basic calculator logic -- unfinished bc i got lazy

--- script.js
+++ script.js
@@ -33,17 +33,52 @@ function operate(operator, a, b) {
 
 }
 
+let numbers = [];
+
 const calcDisplay = document.querySelector('.calc-display');
 
 function clickNumberButton() {
-    if (calcDisplay.textContent === '0') {
+    if (calcDisplay.textContent == '0') {
         calcDisplay.textContent = this.textContent;
     } else {
         calcDisplay.textContent += this.textContent;
     }
 }
 
+function clickOperationButton() {
+    // catch first number for operation (a), store it on `numbers`
+    let firstNumber = calcDisplay.textContent;
+    numbers.push(Number(firstNumber));
+    console.log(numbers);
+
+    calcDisplay.textContent = 0;
+
+    console.log(numbers);
+}
+
+function clickEqualButton() {
+    let secondNumber = calcDisplay.textContent;
+    numbers.push(Number(secondNumber));
+
+    console.log(numbers);
+    findResults();
+}
+
+function findResults() {
+    console.log(add(numbers[0], numbers[1]));
+
+    numbers = [];
+}
+
 const numberButtons = document.querySelectorAll('.btn-number');
 numberButtons.forEach((button) => {
     button.addEventListener("click", clickNumberButton);
 })
+
+const operationButtons = document.querySelectorAll('.btn-operation');
+operationButtons.forEach((button) => {
+    button.addEventListener("click", clickOperationButton);
+})
+
+const equalButton = document.querySelector('.btn-equal');
+equalButton.addEventListener("click", clickEqualButton);
